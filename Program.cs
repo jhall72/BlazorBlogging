ef8fac2f40db0a2b855bf0602837a4dbc126c463
John Hall <mr.johnalexhall@gmail.com>
2026-02-07 23:01:39 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace BlazorBlogging;
+
+public class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var builder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        builder.Services.AddLogging();
+
+        builder.RootComponents.Add<App>("app");
+
+        var app = builder.Build();
+
+        app.MainWindow
+            .SetTitle("BlazorBlogging")
+            .SetUseOsDefaultSize(true);
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.ShowMessage("Fatal Exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
